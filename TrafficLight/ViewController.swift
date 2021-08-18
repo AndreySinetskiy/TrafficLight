d976d6f32c251faee23c7b4494e89a76fa7dc0dd
Andrey Sinetskiy <siniy.85@mail.ru>
2021-08-18 10:49:18 +0300

    logic of change light

--- TrafficLight/ViewController.swift
+++ TrafficLight/ViewController.swift
@@ -9,11 +9,42 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var redLightView: UIView!
+    @IBOutlet weak var yellowLightView: UIView!
+    @IBOutlet weak var greenLightView: UIView!
+    @IBOutlet weak var changeLightButton: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        redLightView.alpha = 0.3
+        redLightView.layer.cornerRadius = 40
+        
+        yellowLightView.alpha = 0.3
+        yellowLightView.layer.cornerRadius = 40
+        
+        greenLightView.alpha = 0.3
+        greenLightView.layer.cornerRadius = 40
+        
+        changeLightButton.layer.cornerRadius = 15
+        
     }
-
-
+    var countPressButton = 0
+    @IBAction func changeLightButtonPressed(_ sender: UIButton) {
+        countPressButton += 1
+        if countPressButton % 3 == 1 {
+        redLightView.alpha = 1
+        yellowLightView.alpha = 0.3
+        greenLightView.alpha = 0.3
+            changeLightButton.setTitle("Next", for: .normal)
+        } else if countPressButton % 3 == 2 {
+            redLightView.alpha = 0.3
+            yellowLightView.alpha = 1
+            greenLightView.alpha = 0.3
+        } else {
+            redLightView.alpha = 0.3
+            yellowLightView.alpha = 0.3
+            greenLightView.alpha = 1
+        }
+    }
+    
 }
 
