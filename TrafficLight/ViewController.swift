b653be70e4106ec6cd77e2063ff3d027746b1238
Andrey Sinetskiy <siniy.85@mail.ru>
2021-08-18 16:38:22 +0300

    optimization of logic

--- TrafficLight/ViewController.swift
+++ TrafficLight/ViewController.swift
@@ -10,37 +10,39 @@ import UIKit
 class ViewController: UIViewController {
 
     @IBOutlet weak var redLightView: UIView!
+    
     @IBOutlet weak var yellowLightView: UIView!
+    
     @IBOutlet weak var greenLightView: UIView!
+    
     @IBOutlet weak var changeLightButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         redLightView.alpha = 0.3
-        redLightView.layer.cornerRadius = redLightView.frame.size.width/2
+        redLightView.layer.cornerRadius = redLightView.frame.size.height / 2
         
         yellowLightView.alpha = 0.3
-        yellowLightView.layer.cornerRadius = yellowLightView.frame.size.width/2
+        yellowLightView.layer.cornerRadius = yellowLightView.frame.size.height / 2
         
         greenLightView.alpha = 0.3
-        greenLightView.layer.cornerRadius = greenLightView.frame.size.width/2
+        greenLightView.layer.cornerRadius = greenLightView.frame.size.height / 2
         
         changeLightButton.layer.cornerRadius = 15
         
     }
-    var countPressButton = 0
+    private var countPressButton = 0
     @IBAction func changeLightButtonPressed(_ sender: UIButton) {
+        changeLightButton.setTitle("Next", for: .normal)
         countPressButton += 1
         if countPressButton % 3 == 1 {
-        redLightView.alpha = 1
-        yellowLightView.alpha = 0.3
-        greenLightView.alpha = 0.3
-            changeLightButton.setTitle("Next", for: .normal)
+            redLightView.alpha = 1
+            greenLightView.alpha = 0.3
         } else if countPressButton % 3 == 2 {
             redLightView.alpha = 0.3
             yellowLightView.alpha = 1
             greenLightView.alpha = 0.3
         } else {
-            redLightView.alpha = 0.3
             yellowLightView.alpha = 0.3
             greenLightView.alpha = 1
         }
